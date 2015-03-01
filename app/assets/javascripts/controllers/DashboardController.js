ffe25cd82a52a9b3223c14cbd01f4202bc7b9255
Adnan Abdulhussein <adnan@bitnami.com>
2015-03-01 09:50:05 -0800

    Frontend changes and docs

--- app/assets/javascripts/controllers/DashboardController.js
+++ app/assets/javascripts/controllers/DashboardController.js
@@ -6,10 +6,14 @@ angular.module('controllers')
   $scope.loadingMessage = (function() {
     var messages = ['Hey, we are just loading your github information',
                     'Just fetching data, we\'ll be with you asap',
-                    'Ooo look, pretty circles']
+                    'Ooo look, pretty circles',
+                    'We\'re waving some wands about',
+                    'Something is happening... I think...',
+                    'Calm down dear, it\'s just a loading screen']
 
     return messages[Math.floor(Math.random() * messages.length)];
   })();
+
 
   RepoService.all().then(function(data) {
     $scope.repos = data;
